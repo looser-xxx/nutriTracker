1864a0323ac6732009b7695d73f84aaee3f2baa0
Suprabhat Maurya <itismaurya4769@gmail.com>
2026-03-16 11:27:00 +0530

    updated the database style for better data storage and prevert server for constant calculations

--- models.py
+++ models.py
@@ -24,6 +24,11 @@ class FoodLog(db.Model):
     __tablename__ = "foodLog"
 
     id = db.Column(db.Integer, primary_key=True)
-    foodId = db.Column(db.Integer, db.ForeignKey("foodDirectory.id"), nullable=False)
+    foodName = db.Column(db.String(100), nullable=False)
     amountInG = db.Column(db.Float, nullable=False)
+    calories = db.Column(db.Float, nullable=False)
+    protein = db.Column(db.Float, nullable=False)
+    carbs = db.Column(db.Float, nullable=False)
+    fat = db.Column(db.Float, nullable=False)
+    fiber = db.Column(db.Float, nullable=False)
     dateLogged = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
